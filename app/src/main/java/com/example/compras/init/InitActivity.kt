d6a0a53bfa85eade3cc972166086dc1c87c43b33
Sergio Rodrigues <sergio.n4todev@gmail.com>
2024-03-23 15:56:31 -0300

    [feature] Subindo Base inicial de navegação HOME OK

--- app/src/main/java/com/example/compras/init/InitActivity.kt
+++ app/src/main/java/com/example/compras/init/InitActivity.kt
@@ -1,21 +1,49 @@
 package com.example.compras.init
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.compras.R
+import com.example.compras.databinding.ActivityInitBinding
+import com.example.compras.main.MainActivity
 
 class InitActivity : AppCompatActivity() {
+    private val binding get() = _binding!!
+    private var _binding: ActivityInitBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_init)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityInitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onClick()
+        onValidate()
+
+    }
+
+    private fun onClick() {
+        with(binding){
+            btnNext.setOnClickListener {
+                val intent = Intent(this@InitActivity, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+
         }
+
+
+    }
+
+    private fun onValidate(): Boolean{
+
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/compras/main/MainActivity.kt
+++ app/src/main/java/com/example/compras/main/MainActivity.kt
@@ -1,21 +1,50 @@
 package com.example.compras.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.compras.R
+import com.example.compras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+        supportActionBar?.hide()
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.containerPai) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        binding.navigationMenu.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _ , destination, _ ->
+            when(destination.id){
+                R.id.homeFragment -> binding.navigationMenu.visibility = View.VISIBLE
+                R.id.listFragment -> binding.navigationMenu.visibility = View.VISIBLE
+                R.id.historicFragment -> binding.navigationMenu.visibility = View.VISIBLE
+                else -> binding.navigationMenu.visibility = View.GONE
+            }
+
         }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
